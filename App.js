1ca4b668fd4eb0bf83872e16a7e756c7d8606e77
Qappture <qappture@gmail.com>
2018-10-13 14:36:17 -0500

    TabNavigator has been deprecated
    Had to switch to createBottomTabNavigator and also had to use const as root rather than embedding in <View>

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
+//
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+//
+import AuthScreen from 'app/screens/AuthScreen';
+import WelcomeScreen from 'app/screens/WelcomeScreen';
+
+//
+class App extends React.Component {
+
+  //
   render() {
+
+    //
+    const MainNavigator = createBottomTabNavigator({
+        Welcome: { screen: WelcomeScreen },
+        Auth: { screen: AuthScreen },
+    });
+
+    //
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MainNavigator />
     );
+
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
